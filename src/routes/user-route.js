43433ad525c9ed45788cb0063ec98aa42cea32c5
NoPalm0il <jpedrocr2000@hotmail.com>
2020-06-04 12:10:39 +0100

    commented code, usefull for the future

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -5,8 +5,9 @@ const authorize = require("../configs/authorization");
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 
+/*
 router.get("/book", authorize(), userController.getBooks);
 router.post("/book/:id", authorize(), userController.addBook);
 router.delete("/book/:id", authorize(), userController.removeBook);
-
+*/
 module.exports = router;
