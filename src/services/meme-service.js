946e8dd63c88e9e1c2f96f93b11d0188215a72c0
TMurteira <tiagomurteira2000@gmail.com>
2020-07-12 16:54:22 +0100

    Updated meme-service and meme-mongodb

--- src/services/meme-service.js
+++ src/services/meme-service.js
@@ -1,93 +1,33 @@
-const db = require("../configs/mongodb.js").getDB();
-//const store = require("../configs/minio.js");
-const ObjectId = require("mongodb").ObjectID;
+const memes = require ('./memes.json');
 
-exports.getMemes = (queryString) => {
-  return new Promise((resolve, reject) => {
-    let filter = {};
-    if (queryString.search) {
-      filter.title = { $regex: new RegExp(queryString.search, "i") };
-    }
-    db.collection("memes")
-      .find(filter)
-      .project({ title: 1, author: 1 })
-      .toArray()
-      .then((memes) => resolve(memes))
-      .catch((err) => reject(err));
+exports.getBooks = () => {
+  return new Promise ((resolve, reject) => {
+    resolve (
+      memes.map (meme => ({
+        _id: meme._id,
+        title: meme.title,
+        author: meme.author,
+      }))
+    );
   });
 };
-exports.getMeme = (id) => {
-  return new Promise((resolve, reject) => {
-    db.collection("memes")
-      .findOne({ _id: ObjectId(id) })
-      .then((meme) => resolve(meme))
-      .catch((err) => reject(err));
+exports.getMeme = id => {
+  return new Promise ((resolve, reject) => {
+    resolve (memes.find (meme => meme._id == id));
   });
 };
-exports.insertMemes = (body) => {
-  return new Promise((resolve, reject) => {
-    db.collection("memes")
-      .insertOne({
-        title: body.title,
-        category: body.category,
-        author: body.author,
-        publish: body.publish,
-      })
-      .then((res) => resolve({ inserted: 1, _id: res.insertedId }))
-      .catch((err) => reject(err));
+exports.insertMeme = body => {
+  return new Promise ((resolve, reject) => {
+    resolve ({inserted: 1});
   });
 };
-
 exports.updateMeme = (id, body) => {
-  return new Promise((resolve, reject) => {
-    db.collection("memes")
-      .updateOne(
-        { _id: ObjectId(id) },
-        {
-          $set: {
-            title: body.title,
-            category: body.category,
-            author: body.author,
-            publish: body.publish,
-          },
-        }
-      )
-      .then(() => resolve({ updated: 1 }))
-      .catch((err) => reject(err));
+  return new Promise ((resolve, reject) => {
+    resolve ({updated: 1});
   });
 };
-
-/*
-exports.updateBookCover = (id, file) => {
-  return new Promise((resolve, reject) => {
-    let url = "";
-    db.collection("books")
-      .findOne({ _id: ObjectId(id) })
-      .then((book) => {
-        let promises = [store.uploadFile(file.path, file.type)];
-        if (book.cover) {
-          const aux = book.cover.split("?")[0].split("/");
-          promises.push(store.removeFile(aux[aux.length - 1]));
-        }
-        return Promise.all(promises);
-      })
-      .then(([presignedUrl, deleted]) => {
-        url = presignedUrl;
-        return db.collection("books").updateOne({ _id: ObjectId(id) }, { $set: { cover: presignedUrl } });
-      })
-      .then(() => {
-        resolve({ updated: 1, url });
-      })
-      .catch((err) => reject(err));
-  });
-};
-*/
-
-exports.removeMeme = (id) => {
-  return new Promise((resolve, reject) => {
-    db.collection("memes")
-      .deleteOne({ _id: ObjectId(id) })
-      .then(() => resolve({ removed: 1 }))
-      .catch((err) => reject(err));
+exports.removeMeme = id => {
+  return new Promise ((resolve, reject) => {
+    resolve ({removed: 1});
   });
 };
